dc335698aa984370f7ff24d8f458fd82fd7b988d
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2019-02-09 22:49:46 +0400

    Added Collection View

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Sergey Neupokoev on 9/2/19.
+//  Copyright © 2019 Sergey Neupokoev. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
